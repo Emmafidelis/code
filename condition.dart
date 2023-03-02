2c7e558cd176d3754000931324b44a07bcd0644e
Emanuel Fidelis <125068974+Emmafidelis@users.noreply.github.com>
2023-03-02 12:53:20 +0300

    Add files via upload

---
+++ condition.dart
@@ -0,0 +1,12 @@
+void main() {
+  var num = 15;
+  if (num > 10) {
+    print("$num is greater than 10");
+  } else if (num < 10) {
+    print("$num is less than 10");
+  } else if (num == 10) {
+    print("$num is equal to 10");
+  } else {
+    print("Invalid number");
+  }
+}
